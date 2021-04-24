76afb23d9b2f4025a691477ac9d55e8d9388e6e0
hackedak <s.athulkrishnan100@gmail.com>
2021-04-25 00:02:26 +0530

    added to heroku

--- src/controllers/welcomeController.js
+++ src/controllers/welcomeController.js
@@ -3,7 +3,6 @@ const {welcomeMsg} = require('../embeds/exports')
 
 function WelcomeGreeting(member) {
     const channel = member.guild.channels.cache.find(member => member.name === 'welcome');
-console.log(channel);
     const greetingMsg = new welcomeMsg(channel)
     greetingMsg.Welcome(member)
 }
